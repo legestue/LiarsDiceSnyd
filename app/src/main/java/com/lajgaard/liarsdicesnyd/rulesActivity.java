221473c844ed06bf95b6605dbdfbb7ec81d019c4
lajgaard <13120@hih.au.dk>
2016-11-30 10:34:11 +0100

    The spinner is added to rule activity to change explanation of the rule

--- app/src/main/java/com/lajgaard/liarsdicesnyd/rulesActivity.java
+++ app/src/main/java/com/lajgaard/liarsdicesnyd/rulesActivity.java
@@ -4,13 +4,48 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class rulesActivity extends AppCompatActivity {
+
+    Spinner spinneruse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
+        spinneruse = (Spinner)findViewById(R.id.spinner);
+        spinneruse.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0 :
+                        //adding toast in each spinner item. You can use any code here
+                        Toast.makeText(getApplicationContext(), "Over all rules selected", Toast.LENGTH_LONG).show();
+                        break;
+                    case 1 :
+                        Toast.makeText(getApplicationContext(), "How to count eyes selected", Toast.LENGTH_LONG).show();
+                        break;
+                    case 2 :
+                        Toast.makeText(getApplicationContext(), "How the bids works", Toast.LENGTH_LONG).show();
+                        break;
+                    case 3 :
+                        Toast.makeText(getApplicationContext(), "How the stair works selected", Toast.LENGTH_LONG).show();
+                        break;
+                    default:
+
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //Not required now
+            }
+        });
     }
 
     public void exitRules(View view) //the same as the other way of changing activity
